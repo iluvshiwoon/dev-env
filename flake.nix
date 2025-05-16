d31d5a2ff044a46a7df589aee553839a3e82b9e7
iluvshiwoon <sntcillian@gmail.com>
2025-05-16 18:36:29 +0800

    flake for template access

--- flake.nix
+++ flake.nix
@@ -95,7 +95,7 @@
                 --experimental-features 'nix-command flakes' \
                 flake init \
                 --template \
-                "github:the-nix-way/dev-templates#''${TEMPLATE}"
+                "github:iluvshiwoon/dev-env#''${TEMPLATE}"
             '';
           };
         }
