a5022ac2197e50735e1468f14ccfcbbf54b197fb
iluvshiwoon <sntcillian@gmail.com>
2025-05-17 18:28:57 +0800

    fmt

--- flake.nix
+++ flake.nix
@@ -3,86 +3,86 @@
 
   inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
 
-  outputs = inputs:
-    let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f: inputs.nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import inputs.nixpkgs { inherit system; };
-      });
-
-      scriptDrvs = forEachSupportedSystem ({ pkgs }:
-        let
-          getSystem = "SYSTEM=$(nix eval --impure --raw --expr 'builtins.currentSystem')";
-          forEachDir = exec: ''
-            for dir in */; do
-              (
-                cd "''${dir}"
-
-                ${exec}
-              )
-            done
-          '';
-        in
-        {
-          format = pkgs.writeShellApplication {
-            name = "format";
-            runtimeInputs = with pkgs; [ nixpkgs-fmt ];
-            text = ''
-              shopt -s globstar
-
-              nixpkgs-fmt -- **/*.nix
-            '';
-          };
-
-          # only run this locally, as Actions will run out of disk space
-          build = pkgs.writeShellApplication {
-            name = "build";
-            text = ''
-              ${getSystem}
-
-              ${forEachDir ''
-                echo "building ''${dir}"
-                nix build ".#devShells.''${SYSTEM}.default"
-              ''}
-            '';
-          };
-
-          check = pkgs.writeShellApplication {
-            name = "check";
-            text = forEachDir ''
-              echo "checking ''${dir}"
-              nix flake check --all-systems --no-build
-            '';
-          };
-
-          update = pkgs.writeShellApplication {
-            name = "update";
-            text = forEachDir ''
-              echo "updating ''${dir}"
-              nix flake update
-            '';
-          };
+  outputs = inputs: let
+    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forEachSupportedSystem = f:
+      inputs.nixpkgs.lib.genAttrs supportedSystems (system:
+        f {
+          pkgs = import inputs.nixpkgs {inherit system;};
         });
-    in
+
+    scriptDrvs = forEachSupportedSystem ({pkgs}: let
+      getSystem = "SYSTEM=$(nix eval --impure --raw --expr 'builtins.currentSystem')";
+      forEachDir = exec: ''
+        for dir in */; do
+          (
+            cd "''${dir}"
+
+            ${exec}
+          )
+        done
+      '';
+    in {
+      format = pkgs.writeShellApplication {
+        name = "format";
+        runtimeInputs = with pkgs; [nixpkgs-fmt];
+        text = ''
+          shopt -s globstar
+
+          nixpkgs-fmt -- **/*.nix
+        '';
+      };
+
+      # only run this locally, as Actions will run out of disk space
+      build = pkgs.writeShellApplication {
+        name = "build";
+        text = ''
+          ${getSystem}
+
+          ${forEachDir ''
+            echo "building ''${dir}"
+            nix build ".#devShells.''${SYSTEM}.default"
+          ''}
+        '';
+      };
+
+      check = pkgs.writeShellApplication {
+        name = "check";
+        text = forEachDir ''
+          echo "checking ''${dir}"
+          nix flake check --all-systems --no-build
+        '';
+      };
+
+      update = pkgs.writeShellApplication {
+        name = "update";
+        text = forEachDir ''
+          echo "updating ''${dir}"
+          nix flake update
+        '';
+      };
+    });
+  in
     {
-      devShells = forEachSupportedSystem ({ pkgs }: {
+      devShells = forEachSupportedSystem ({pkgs}: {
         default = pkgs.mkShell {
-          packages =
-            with scriptDrvs.${pkgs.system}; [
+          packages = with scriptDrvs.${pkgs.system};
+            [
               build
               check
               format
               update
-            ] ++ [ pkgs.nixpkgs-fmt ];
+            ]
+            ++ [pkgs.nixpkgs-fmt];
         };
       });
 
-      packages = forEachSupportedSystem ({ pkgs }:
-        rec {
+      packages = forEachSupportedSystem (
+        {pkgs}: rec {
           default = dvt;
           dvt = pkgs.writeShellApplication {
             name = "dvt";
-            bashOptions = [ "errexit" "pipefail" ];
+            bashOptions = ["errexit" "pipefail"];
             text = ''
               if [ -z "''${1}" ]; then
                 echo "no template specified"
@@ -101,13 +101,9 @@
         }
       );
     }
-
-    //
-
-    {
+    // {
       templates = rec {
-
-               c = {
+        c = {
           path = ./c;
           description = "C/C++ development environment";
         };
