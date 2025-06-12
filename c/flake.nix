2066039d6ef13ffdb1a8cf18e7a236624e872ef6
iluvshiwoon <sntcillian@gmail.com>
2025-06-12 20:17:56 +0800

    test

--- c/flake.nix
+++ c/flake.nix
@@ -1,17 +1,17 @@
 {
   description = "A Nix-flake-based C/C++ development environment";
 
-  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/*";
 
   outputs = inputs: let
     supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
     forEachSupportedSystem = f:
       inputs.nixpkgs.lib.genAttrs supportedSystems (system:
         f {
-          pkgs = import inputs.nixpkgs {inherit system;};
+          pkgs = import inputs.nixpkgs { inherit system; };
         });
   in {
-    devShells = forEachSupportedSystem ({pkgs}: let
+    devShells = forEachSupportedSystem ({ pkgs }: let
       stdenv = pkgs.clangStdenv;
     in {
       default =
@@ -23,19 +23,25 @@
           packages = with pkgs;
             [
               clang-tools
-              valgrind
+              man-pages           # For standard C libc man pages
+              man-pages-posix     # POSIX man pages
             ]
             ++ (
               if system == "aarch64-darwin"
               then []
-              else [gdb]
+              else [gdb valgrind]
             );
+
           env = {
             CLANGD_FLAGS = "--query-driver=${pkgs.lib.getExe stdenv.cc}";
           };
-          # shellHook = ''
-          # '';
+
+          shellHook = ''
+            # Ensure MANPATH includes local documentation
+            export MANPATH="${pkgs.man-pages}/share/man:${pkgs.man-pages-posix}/share/man:${pkgs.valgrind}/share/man:$MANPATH"
+          '';
         };
     });
   };
 }
+
